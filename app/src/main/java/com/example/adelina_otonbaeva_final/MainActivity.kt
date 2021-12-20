1cc183c2a4e38be2a8ef43b80d55b8892fd3256e
adelinaotonbaeva <otonbaeva_a@auca.kg>
2021-12-21 01:39:53 +0600

    final

--- app/src/main/java/com/example/adelina_otonbaeva_final/MainActivity.kt
+++ app/src/main/java/com/example/adelina_otonbaeva_final/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.adelina_otonbaeva_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val constraintLayout =
+            findViewById<ConstraintLayout>(R.id.constraintLayout9)
+        constraintLayout.setOnClickListener{
+            startActivity(Intent(this,SecondActivity::class.java))
+        }
     }
 }
